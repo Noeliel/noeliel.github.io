c6eb2b10b33ab64602aadfa43d859ccfca50f1bf
Noeliel <noelieldev@gmail.com>
2014-11-10 13:10:17 +0100

    Moved some stuff

---
+++ index.php
@@ -0,0 +1,30 @@
+<title>Noeliel - iOS Developer</title>
+<center><font face="Lucida Console">
+<font size="40"><b>Noeliel</b></font>&nbsp;&nbsp;&nbsp;iOS Developer
+<br>
+<br>
+<font size="5"><b>Welcome!</b></font>
+<br>
+<br>
+Seems like you managed to find my site. Unfortunately, there's nothing to see here so far.
+<br>
+But you can head over to my twitter <a href="http://twitter.com/NoelielDEV">@NoelielDEV</a>
+or look for my tweaks / apps on Cydia:
+<br>
+<br>
+<a href="cydia://package/org.thebigboss.cybernetic">Cybernetic</a> - Custom Notification Banner Gestures & more
+<br>
+<a href="cydia://package/com.noeliel.quickpass">QuickPass</a> - Easy & effective way of enabling/disabling your device passcode
+<br>
+<a href="cydia://package/com.noeliel.sliderz">Sliderz</a> - Custom Slider Texts (e.g. replace 'slide to unlock') & more
+<br>
+<br>
+I also write apps for OS X:
+<br>
+<br>
+<a href="downloads/RemoteMusX.zip">RemoteMusX</a> - A frontend for SSHMediaControls (from Cydia ofc :P)
+
+
+
+</center>
+</font>
